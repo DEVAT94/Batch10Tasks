80b772775b7a0e107e7be3a3a59540a3193e8eb0
Alex <al.toth94@gmail.com>
2025-06-10 13:01:15 +0200

    Added  Aufgabe 2

--- Tasks/3.4.2/main.dart
+++ Tasks/3.4.2/main.dart
@@ -1,4 +1,6 @@
 void main(){
+
+  //Aufgabe 1
   int age = 15;
   bool hasParentalConsent = true;
   int movieAgeRating = 16;
@@ -14,5 +16,29 @@ void main(){
     //print(age);
     //print(movieAgeRating);
   }
-    
+
+  //Aufgabe 2
+  bool isLoggedIn = true;
+  bool isBanned = false;
+  bool isSubscripted = true;
+  int age = 18;
+  
+  if(!isLoggedIn){
+    print("Bitte logge dich ein"); 
+  }
+  
+  if(isBanned == true){
+    print("Dein Account wurde gesperrt");
+  }
+  
+  if(!isSubscripted){
+    print("Bitte aboniere");
+  }
+  
+  if(age < 18){
+    print("Du bist zu jung");
+  }
+  else{
+    print("Willkommen");
+  }
 }
