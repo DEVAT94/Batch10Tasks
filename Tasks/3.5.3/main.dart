7cf5cad825d4989222c0ae90b685d7375d18ad4f
DEVAT94 <al.toth94@gmail.com>
2025-06-20 15:37:12 +0200

    Added 3.5.3

---
+++ Tasks/3.5.3/main.dart
@@ -0,0 +1,39 @@
+import 'dart:ffi';
+
+void main(){
+  //Aufgabe 1
+  print(sum(1,2,3));
+
+  //Aufgabe 2
+  print(stringLength('ergebnis'));
+  print(stringLength('der'));
+  print(stringLength('funktion'));
+
+  //Aufgabe 3
+  print(vocals('Alex'));
+  print(vocals('Moritz'));
+  print(vocals('Waschbär'));
+}
+
+//Funktion Aufgabe 1
+int sum(int a, int b, int c){
+  int result = a+b+c;
+  return result;
+}
+
+//Funktion Aufgabe 2
+int stringLength(String word){
+  int result = word.length;
+  return result;
+}
+
+//Funktion Aufgabe 3
+int vocals(String word){
+  int quantity = 0;
+  for(int i = 0; i < word.length; i++){
+    if('aeiouAEIOU'.contains(word[i])){
+      quantity++;
+    }
+  }
+  return quantity;
+}
