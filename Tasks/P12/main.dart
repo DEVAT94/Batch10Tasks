b5f2965c660fe49114792bf0ef3b1c9cce8b7101
DEVAT94 <al.toth94@gmail.com>
2025-07-02 10:19:58 +0200

    rework

--- Tasks/P12/main.dart
+++ Tasks/P12/main.dart
@@ -1,42 +1,32 @@
 abstract class DatabaseRepository{
-  void getRecipe(Get,Set);
+  void recipe(Get,Set);
   void saveRecipe(String recipeName, String recipe, String ingrediens);
-  void getLevel();
-  void setLevel();
+  void level(Get, Set);
   void getExperience();
-  void getTopic();
-  void setTopic();
+  void topic(Get, Set);
   void rating();
 }
 
 class MockDatabaseRepository implements DatabaseRepository{
   @override
-  void getRecipe(Get, Set) {
-    
+  void recipe(Get, Set) {
+    // TODO: implement recipe
   }
   @override
   void saveRecipe(String recipeName, String recipe, String ingrediens) {
     // TODO: implement saveRecipe
   }
   @override
-  void getLevel() {
-    // TODO: implement getLevel
-  }
-  @override
-  void setLevel() {
-    // TODO: implement setLevel
+  void level(Get, Set) {
+    // TODO: implement level
   }
   @override
   void getExperience() {
     // TODO: implement getExperience
   }
   @override
-  void getTopic() {
-    // TODO: implement getTopic
-  }
-  @override
-  void setTopic() {
-    // TODO: implement setTopic
+  void topic(Get, Set) {
+    // TODO: implement topic
   }
   @override
   void rating() {
