1f80ec078b3ddfb65246f27e93d732ea44644ca1
DEVAT94 <al.toth94@gmail.com>
2025-07-16 17:27:35 +0200

    Added GNav

--- Tasks/f_helloworld/f_ChefsArena/f_chefsarena/lib/main.dart
+++ Tasks/f_helloworld/f_ChefsArena/f_chefsarena/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: const Color.fromARGB(255, 74, 195, 112),
         appBar: AppBar(
           backgroundColor: const Color.fromARGB(255, 74, 74, 74),
           centerTitle: true,
@@ -22,29 +22,193 @@ class MainApp extends StatelessWidget {
           ),
         ),
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
-              color: Colors.blueGrey,
-              width: 50,
-              child: Card(color: Colors.blueGrey),
+              height: 200,
+              width: 150,
+              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+              child: Card(
+                elevation: 4,
+                color: Color.fromARGB(255, 74, 74, 74),
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Name: Alex',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                          Text(
+                            'Rang: Hobbykoch',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ],
+                      ),
+
+                      Divider(color: Colors.amber),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 150,
+              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+              child: Card(
+                elevation: 4,
+                color: Color.fromARGB(255, 74, 74, 74),
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Thema des Tages',
+                        style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.lightGreen,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Ein Sommergericht',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 380,
+              width: 150,
+              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+              child: Card(
+                elevation: 4,
+                color: Color.fromARGB(255, 74, 74, 74),
+                child: Padding(
+                  padding: EdgeInsetsGeometry.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Statistiken',
+                        style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Column(
+                        spacing: 2,
+                        children: [
+                          Card(
+                            elevation: 4,
+                            color: const Color.fromARGB(255, 156, 241, 193),
+                            child: Padding(
+                              padding: EdgeInsetsGeometry.only(
+                                left: 160,
+                                top: 40,
+                                right: 160,
+                                bottom: 40,
+                              ),
+                            ),
+                          ),
+                          Card(
+                            elevation: 4,
+                            color: const Color.fromARGB(255, 156, 241, 193),
+                            child: Padding(
+                              padding: EdgeInsetsGeometry.only(
+                                left: 160,
+                                top: 40,
+                                right: 160,
+                                bottom: 40,
+                              ),
+                            ),
+                          ),
+                          Card(
+                            elevation: 6,
+                            color: const Color.fromARGB(255, 156, 241, 193),
+                            child: Padding(
+                              padding: EdgeInsetsGeometry.only(
+                                left: 160,
+                                top: 40,
+                                right: 160,
+                                bottom: 40,
+                              ),
+                              child: Row(
+                                children: [
+                                  Text('Platzhalter')
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.amber,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.add_home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.cookie), label: 'Duell'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.receipt_long_rounded),
-              label: 'Rezepte',
-            ),
-            BottomNavigationBarItem(icon: Icon(Icons.group), label: 'Freunde'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
-          ],
-        ),
+        bottomNavigationBar: BottomNavBarItems(),
       ),
+    );
+  }
+}
+
+class BottomNavBarItems extends StatelessWidget {
+  const BottomNavBarItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GNav(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      backgroundColor: Color.fromARGB(255, 74, 74, 74),
+      //tabMargin: EdgeInsetsGeometry.zero,
+      tabs: [
+        GButton(
+          icon: Icons.add_home,
+          iconColor: Colors.white,
+          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
+          textColor: Colors.white,
+          hoverColor: Colors.yellow,
+          text: 'Home',
+          style: GnavStyle.oldSchool,
+        ),
+        GButton(
+          icon: Icons.cookie,
+          iconColor: Colors.white,
+          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
+          textColor: Colors.white,
+          hoverColor: Colors.yellow,
+          text: 'Duell',
+        ),
+        GButton(
+          icon: Icons.receipt_long_rounded,
+          iconColor: Colors.white,
+          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
+          activeBorder: Border.symmetric(),
+          textColor: Colors.white,
+          hoverColor: Colors.yellow,
+          text: 'Rezepte',
+        ),
+        GButton(
+          icon: Icons.group,
+          iconColor: Colors.white,
+          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
+          textColor: Colors.white,
+          hoverColor: Colors.yellow,
+          text: 'Freunde',
+        ),
+      ],
     );
   }
 }
