61ba5c94e03e882a30e80f1f973021aab4a368d8
DEVAT94 <al.toth94@gmail.com>
2025-07-17 15:06:24 +0200

    Worked on classes and layout

--- Tasks/f_helloworld/f_ChefsArena/f_chefsarena/lib/main.dart
+++ Tasks/f_helloworld/f_ChefsArena/f_chefsarena/lib/main.dart
@@ -11,159 +11,181 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 74, 195, 112),
         appBar: AppBar(
+          elevation: 40,
           backgroundColor: const Color.fromARGB(255, 74, 74, 74),
           centerTitle: true,
-          title: Text(
+          title: const Text(
             'Home',
             style: TextStyle(color: Color.fromARGB(235, 255, 255, 255)),
           ),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Container(
-              height: 200,
-              width: 150,
-              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-              child: Card(
-                elevation: 4,
-                color: Color.fromARGB(255, 74, 74, 74),
-                child: Padding(
-                  padding: EdgeInsetsGeometry.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Name: Alex',
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                          Text(
-                            'Rang: Hobbykoch',
-                            style: TextStyle(fontSize: 20, color: Colors.white),
-                          ),
-                        ],
-                      ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 12,
+                ),
 
-                      Divider(color: Colors.amber),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              width: 150,
-              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-              child: Card(
-                elevation: 4,
-                color: Color.fromARGB(255, 74, 74, 74),
-                child: Padding(
-                  padding: EdgeInsetsGeometry.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Thema des Tages',
-                        style: TextStyle(
-                          fontSize: 28,
-                          color: Colors.lightGreen,
-                          fontWeight: FontWeight.bold,
+                /*
+                
+                Bereich: Profildaten
+
+                */
+                child: Card(
+                  elevation: 4,
+                  color: const Color.fromARGB(255, 74, 74, 74),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 100),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name: Alex',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20, color: Colors.white),
                         ),
-                      ),
-                      Text(
-                        'Ein Sommergericht',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: 380,
-              width: 150,
-              margin: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-              child: Card(
-                elevation: 4,
-                color: Color.fromARGB(255, 74, 74, 74),
-                child: Padding(
-                  padding: EdgeInsetsGeometry.all(10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Statistiken',
-                        style: TextStyle(
-                          fontSize: 28,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+                        Text(
+                          'Rang: Hobbykoch',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
                         ),
-                      ),
-                      Column(
-                        spacing: 2,
-                        children: [
-                          Card(
-                            elevation: 4,
-                            color: const Color.fromARGB(255, 156, 241, 193),
-                            child: Padding(
-                              padding: EdgeInsetsGeometry.only(
-                                left: 160,
-                                top: 40,
-                                right: 160,
-                                bottom: 40,
+                        SizedBox(height: 10),
+                        Container(
+                          padding: EdgeInsets.only(right: 100),
+                          child: Column(
+                            children: [
+                              Text(
+                                'Erfahrungspunkte: 50 XP',
+                                textAlign: TextAlign.justify,
+                                style: TextStyle(color: Colors.white),
                               ),
-                            ),
+                              LinearProgressIndicator(color: Colors.amber),
+                            ],
                           ),
-                          Card(
-                            elevation: 4,
-                            color: const Color.fromARGB(255, 156, 241, 193),
-                            child: Padding(
-                              padding: EdgeInsetsGeometry.only(
-                                left: 160,
-                                top: 40,
-                                right: 160,
-                                bottom: 40,
-                              ),
-                            ),
-                          ),
-                          Card(
-                            elevation: 6,
-                            color: const Color.fromARGB(255, 156, 241, 193),
-                            child: Padding(
-                              padding: EdgeInsetsGeometry.only(
-                                left: 160,
-                                top: 40,
-                                right: 160,
-                                bottom: 40,
-                              ),
-                              child: Row(
-                                children: [
-                                  Text('Platzhalter')
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              Container(
+                height: 100,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 12,
+                ),
+
+                /*
+                
+                Bereich: Thema des Tages
+
+                */
+                child: Card(
+                  elevation: 4,
+                  color: const Color.fromARGB(255, 74, 74, 74),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Thema des Tages',
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: Colors.lightGreen,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          'Ein Sommergericht',
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              /*
+                
+                Bereich: Statistiken
+
+                */
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 12,
+                ),
+                child: Card(
+                  elevation: 4,
+                  color: const Color.fromARGB(255, 74, 74, 74),
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(color: Colors.red, height: 10),
+                        Text(
+                          'Statistiken',
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+
+                        /*
+                
+                Bereich: Statistik-Cards
+
+                */
+                        SizedBox(height: 20),
+                        CustomCard(
+                          description: 'Anzahl der gekochten Gerichte:',
+                          subTextDescription: '10',
+                        ),
+                        SizedBox(height: 10),
+                        CustomCard(
+                          description: 'Durchschnittsbewertung:',
+                          subTextDescription: '4 von 10',
+                        ),
+                        SizedBox(height: 10),
+                        CustomCard(
+                          description: 'Lieblingsgegner:',
+                          subTextDescription: 'Nico',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-        bottomNavigationBar: BottomNavBarItems(),
+
+        /*
+                
+                Bereich: Bottom Google NavBar
+
+                */
+        bottomNavigationBar: const BottomNavBarItems(),
       ),
     );
   }
 }
 
+//Google NavBar Class
 class BottomNavBarItems extends StatelessWidget {
   const BottomNavBarItems({super.key});
 
@@ -171,13 +193,12 @@ class BottomNavBarItems extends StatelessWidget {
   Widget build(BuildContext context) {
     return GNav(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      backgroundColor: Color.fromARGB(255, 74, 74, 74),
-      //tabMargin: EdgeInsetsGeometry.zero,
-      tabs: [
+      backgroundColor: const Color.fromARGB(255, 74, 74, 74),
+      tabs: const [
         GButton(
           icon: Icons.add_home,
           iconColor: Colors.white,
-          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
+          iconActiveColor: Color.fromARGB(255, 74, 195, 112),
           textColor: Colors.white,
           hoverColor: Colors.yellow,
           text: 'Home',
@@ -186,7 +207,7 @@ class BottomNavBarItems extends StatelessWidget {
         GButton(
           icon: Icons.cookie,
           iconColor: Colors.white,
-          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
+          iconActiveColor: Color.fromARGB(255, 74, 195, 112),
           textColor: Colors.white,
           hoverColor: Colors.yellow,
           text: 'Duell',
@@ -194,8 +215,7 @@ class BottomNavBarItems extends StatelessWidget {
         GButton(
           icon: Icons.receipt_long_rounded,
           iconColor: Colors.white,
-          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
-          activeBorder: Border.symmetric(),
+          iconActiveColor: Color.fromARGB(255, 74, 195, 112),
           textColor: Colors.white,
           hoverColor: Colors.yellow,
           text: 'Rezepte',
@@ -203,12 +223,51 @@ class BottomNavBarItems extends StatelessWidget {
         GButton(
           icon: Icons.group,
           iconColor: Colors.white,
-          iconActiveColor: const Color.fromARGB(255, 74, 195, 112),
+          iconActiveColor: Color.fromARGB(255, 74, 195, 112),
           textColor: Colors.white,
           hoverColor: Colors.yellow,
           text: 'Freunde',
         ),
       ],
+    );
+  }
+}
+
+//CustomCard Class
+class CustomCard extends StatelessWidget {
+  final String description;
+  final String subTextDescription;
+
+  const CustomCard({
+    super.key,
+    required this.description,
+    required this.subTextDescription,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      color: const Color.fromARGB(255, 156, 241, 193),
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                description,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                subTextDescription,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
