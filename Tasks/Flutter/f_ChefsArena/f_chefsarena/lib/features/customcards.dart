d1d9dfb909494782410266ce955fcab52f80ddb6
DEVAT94 <al.toth94@gmail.com>
2025-08-05 17:47:15 +0200

    CustomCard edited

--- Tasks/Flutter/f_ChefsArena/f_chefsarena/lib/features/customcards.dart
+++ Tasks/Flutter/f_ChefsArena/f_chefsarena/lib/features/customcards.dart
@@ -3,22 +3,28 @@ import 'package:flutter/material.dart';
 class CustomCard extends StatelessWidget {
   final String description;
   final String subTextDescription;
-  final Color color;
+  final Color backgroundColor;
+  final Color foregroundDescription;
+  final Color foregroundSubText;
+  final double width;
 
   const CustomCard({
     super.key,
     required this.description,
     required this.subTextDescription,
-    this.color = const Color.fromARGB(150, 74, 74, 74)
+    this.backgroundColor = const Color.fromARGB(150, 74, 74, 74),
+    this.width = double.infinity, 
+    this.foregroundDescription = Colors.amber, 
+    this.foregroundSubText = Colors.white
   });
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 2,
-      color: color,
+      color: backgroundColor,
       child: SizedBox(
-        width: double.infinity,
+        width: width,
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(
@@ -26,15 +32,15 @@ class CustomCard extends StatelessWidget {
             children: [
               Text(
                 description,
-                style: const TextStyle(
-                  color: Colors.amber,
+                style: TextStyle(
+                  color: foregroundDescription,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               Text(
                 subTextDescription,
-                style: const TextStyle(
-                  color: Colors.white,
+                style: TextStyle(
+                  color: foregroundSubText,
                   fontWeight: FontWeight.bold,
                 ),
               ),
